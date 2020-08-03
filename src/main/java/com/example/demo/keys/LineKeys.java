1ec3ba339e0ef758384f394484019e7fc6ebaed0
jack19990504 <jack19990504@gmail.com>
2020-08-03 18:41:24 +0800

    fix

--- src/main/java/com/example/demo/keys/LineKeys.java
+++ src/main/java/com/example/demo/keys/LineKeys.java
@@ -1,5 +1,5 @@
 package com.example.demo.keys;
 
 public interface LineKeys {
-	String accessToken = "Fete4NBk12TWruvw3FYEqckZnSXZQJQgA3MbobnTeLWXqKhHdA+wfgvxehVaFHGv9Oa+8DfXAaltLgGQfGBDY6qTHTRNtTMoMxZ4P/Hi+Ng62sWiPVmWNmevv8DRAWiN0oSgukSGW9ojHCQM2/n20QdB04t89/1O/w1cDnyilFU=";
+	String accessToken = "your AccessToken here";
 }
