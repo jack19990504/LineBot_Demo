2a2782dae671607685488df42f96fe973fc315c3
jack19990504 <jack19990504@gmail.com>
2020-12-25 16:39:12 +0800

    test again

--- src/main/java/com/example/demo/slack/service/SlackService.java
+++ src/main/java/com/example/demo/slack/service/SlackService.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
-@PropertySource("classpath:slack.properties")
 public class SlackService {
 
     private final Slack slack;
